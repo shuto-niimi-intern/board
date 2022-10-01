d3d4ba0a6b5b4f0595d240b96c90e2e4536b237b
ooitanojohn <qmkh733310@gmail.com>
2022-10-01 22:43:55 +0900

    [fix] tinyint(11) → (1)

--- seed.sql
+++ seed.sql
@@ -7,6 +7,7 @@
 
 -- テーブル削除
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS board;
 
 -- ユーザ
 CREATE TABLE users
@@ -14,12 +15,21 @@ CREATE TABLE users
 	id varchar(255) NOT NULL COMMENT 'ユーザID兼ログインID',
   name varchar(255) DEFAULT 'マツコ・デラックス' COMMENT 'ハンドルネーム',
 	password varchar(255) NOT NULL COMMENT 'パスワード',
-	auth tinyint(11) UNSIGNED DEFAULT 1 NOT NULL COMMENT '権限 :
+	auth tinyint(1) UNSIGNED DEFAULT 1 NOT NULL COMMENT '権限 :
 0=管理者
 1=一般',
 	PRIMARY KEY (id)
 ) COMMENT = 'ユーザ';
 
+-- 単一掲示板
+CREATE TABLE boards
+(
+  id int(11) NOT
+)
+
+
 -- ユーザデータ挿入
 INSERT INTO users (id, password, auth) VALUES ('aiueo', 'pass', '1');
+INSERT INTO users (id, password, auth) VALUES ('apple', 'pass', '1');
+INSERT INTO users (id, password, auth) VALUES ('polca', 'pass', '1');
 

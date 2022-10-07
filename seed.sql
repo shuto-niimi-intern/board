e67118916dde439f9243f41f043618ed3f037550
shutoniimi <114894212+shuto-niimi-intern@users.noreply.github.com>
2022-10-07 13:20:50 +0900

    Update seed.sql
    修正

--- seed.sql
+++ seed.sql
@@ -12,40 +12,30 @@ DROP TABLE IF EXISTS users;
 -- ユーザ
 CREATE TABLE users
 (
-	id varchar(255) NOT NULL COMMENT 'ユーザID兼ログインID',
-  name varchar(255) DEFAULT 'マツコ・デラックス' COMMENT 'ハンドルネーム',
+	id varchar(255) CHARACTER SET ascii NOT NULL COMMENT 'ユーザID兼ログインID',
+  	name varchar(191) DEFAULT 'マツコ・デラックス' COMMENT 'ハンドルネーム',
 	password varchar(255) NOT NULL COMMENT 'パスワード',
-	auth tinyint(1) UNSIGNED DEFAULT 1 NOT NULL COMMENT '権限 :
-0=管理者
-1=一般',
+	auth tinyint(1) UNSIGNED DEFAULT 1 NOT NULL COMMENT '権限 :0=管理者 :1=一般',
 	PRIMARY KEY (id)
 ) COMMENT = 'ユーザ';
 
 -- Re:テーブル
 -- レス先本文はajaxでクリック時取得
-CREATE TABLE res
-(
-  id int(11) NOT NULL AUTO_INCREMENT COMMENT 'コメントかレスID',
-  content text NOT NULL COMMENT '本文',
-  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '投稿日時',
-　updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
-　user_id varchar(255) NOT NULL COMMENT 'ユーザID兼ログインID',
-  res_id int(11) UNSIGNED NOT NULL COMMENT 'レス先ID : 0=コメント id=レス',
-  PRIMARY KEY (id)
-) COMMENT = '掲示板';
-
 -- コメント1 対 レス多
-ALTER TABLE res
-  ADD FOREIGN KEY (user_id)
-  REFERENCES users (id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE
-;
--- user_id,res_idにINDEXを作成
-ALTER TABLE res ADD INDEX res_id_index (res_id);
-ALTER TABLE res ADD INDEX user_id_index (user_id);
+CREATE TABLE `res` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'コメントかレスID',
+  `content` text COLLATE utf8mb4_bin NOT NULL COMMENT '本文',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '投稿日時',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
+  `user_id` varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'ユーザID兼ログインID',
+  `res_id` int(11) unsigned NOT NULL COMMENT 'レス先ID : 0=コメント id=レス',
+  PRIMARY KEY (`id`),
+  KEY `res_id_index` (`res_id`),
+  KEY `user_id_index` (`user_id`),
+  CONSTRAINT `res_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_bbs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='掲示板'
 
--- トランザクション開始
+-- トランザクション開始 トランザクションは動いてるシステムでいい
 BEGIN;
 
 -- ユーザデータ挿入
